ac4bfd7ecb8a627569033df5cecdb6854787d237
IshwarSingh123 <ishwarsinghmars@gmail.com>
2025-01-22 22:03:04 +0530

    Add the file volume of earth

--- VolumeOfEarth.cs
+++ VolumeOfEarth.cs
@@ -1,10 +1,3 @@
-/*
-7. Write a Program to compute the volume of Earth in km^3 and miles^3
-Hint: Volume of a Sphere is (4/3) * pi * r^3 and radius of earth is 6378 km
-O/P => The volume of earth in cubic kilometers is ____ and cubic miles is ____
-
-*/
-
 using System;
 
 class VolumeOfEarth
