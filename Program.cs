d405d48bb118c94161c9587f90d70befb62d947a
Ishwar@123 <ishwarsinghmars@gmail.com>
2025-02-04 14:32:04 +0530

    Add a file for program

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+//using System;
+//using Opps;
+
+
+//Circle circle1 = new Circle();
+//circle1.Display();
+
+//Console.WriteLine("\nEnter the radius of the circle:");
+//double radius = double.Parse(Console.ReadLine());
+
+//// Using parameterized constructor
+//Circle circle2 = new Circle(radius);
+//circle2.Display();
+
+
+
+//Console.WriteLine("Enter the name of the person: ");
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Enetr the age of the person: ");
+//int age = Console.ReadLine();
+
+//Console.WriteLine("Enetr the id of the person: ");
+//int id = Console.ReadLine();
+
+//Console.WriteLine("Enter the address of the person: ");
+//string address = Console.ReadLine();
+
+//Person person = new Person(name,age,id,address);
+//person.Dispaly();
+
+//Person person2 = new Person(person1);
+//Console.WriteLine("\nCopied Person:");
+//person2.Dispaly();
+
