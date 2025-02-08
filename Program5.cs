88ad1c14153db5e06e240371798539b3bc25ae24
Ishwar@123 <ishwarsinghmars@gmail.com>
2025-02-08 19:56:34 +0530

    Smart Home Devices

---
+++ Program5.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+
+//class Device
+//{
+//    public int DeviceId { get; set; }
+//    public string Status { get; set; }
+
+//    public Device(int deviceId, string status)
+//    {
+//        DeviceId = deviceId;
+//        Status = status;
+//    }
+
+//    public virtual void DisplayStatus()
+//    {
+//        Console.WriteLine($"Device ID: {DeviceId}, Status: {Status}");
+//    }
+//}
+
+//class Thermostat : Device
+//{
+//    public double TemperatureSetting { get; set; }
+
+//    public Thermostat(int deviceId, string status, double temperatureSetting)
+//        : base(deviceId, status)
+//    {
+//        TemperatureSetting = temperatureSetting;
+//    }
+
+//    public override void DisplayStatus()
+//    {
+//        base.DisplayStatus();
+//        Console.WriteLine($"Temperature Setting: {TemperatureSetting}°C");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Thermostat thermostat = new Thermostat(101, "Online", 22.5);
+//        thermostat.DisplayStatus();
+//    }
+//}
